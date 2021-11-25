73b42421bf63cb00287b6856ac2309c38a82d5dd
Kevin Jo <kevinhjo725@gmail.com>
2021-11-25 00:21:15 -0800

    Add signup form image

--- frontend/src/components/splash.jsx
+++ frontend/src/components/splash.jsx
@@ -6,7 +6,6 @@ class Splash extends React.Component {
       <div id='splash-page'>
         <div id='splash-head'>
           <h1>Welcome to AquaLog</h1>
-          
         </div>
       </div>
     )
